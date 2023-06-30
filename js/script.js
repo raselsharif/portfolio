e1d1a34159c82c963f8cce266426de5c81bd20ba
raselsharif <raselsharif9@gmail.com>
2023-06-30 22:34:39 +0600

    add form validation and change logo

--- js/script.js
+++ js/script.js
@@ -88,8 +88,68 @@ $(".nav-link").click(function(){
     $(this).addClass("btn__active").siblings().removeClass("btn__active");
   })
 
-
 })
+
+// ========= form validation ======
+let username = document.querySelector(".name");
+let email = document.querySelector(".email");
+let subject = document.querySelector(".subject");
+let text = document.querySelector(".text");
+let btn = document.querySelector(".form__btn");
+let msg = document.querySelector(".error__msg");
+let mail_valid = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+let nameerr = document.querySelector(".name__err");
+let mailerr = document.querySelector(".mail__err");
+let suberr = document.querySelector(".sub__err");
+let texterr = document.querySelector(".text__err");
+
+btn.addEventListener("click", function(){
+  if (username.value === ""){
+    nameerr.innerHTML= "Type your full name";
+    nameerr.style.color="red";
+    username.style.borderBottom = "2px solid red";
+  }else{
+    nameerr.innerHTML= "";
+  }
+
+  if (email.value === ""){
+    mailerr.innerHTML= "Type your valid email";
+    mailerr.style.color="red";
+  }else{
+    if(email.value.match(mail_valid)){
+      mailerr.innerHTML= "";
+    }else{
+    mailerr.innerHTML= "Your email is not valid";
+
+    }
+  }
+
+  if (subject.value === ""){
+    suberr.innerHTML= "Type a subject";
+    suberr.style.color="red";
+  }else{
+    suberr.innerHTML= "";
+  }
+
+  if (text.value === ""){
+    texterr.innerHTML= "Type your message";
+    texterr.style.color="red";
+  }else{
+    texterr.innerHTML= "";
+  }
+
+  // Check if all fields are valid before showing the alert message
+  if (nameerr.innerHTML === "" && mailerr.innerHTML === "" && suberr.innerHTML === "" && texterr.innerHTML === "") {
+    alert("Form submitted successfully!");
+    // Clear input fields
+    username.value = "";
+    email.value = "";
+    subject.value = "";
+    text.value = "";
+  }
+});
+
 
 // ============= slick slider ==========
 $('.slick__slider').slick({
@@ -113,7 +173,7 @@ $('.slick__slider').slick({
   ]
 });
 
-// ========= venobox youtube veido =======
+// ========= venobox youtube video =======
 new VenoBox({
   selector: '.my-video-links',
 });
