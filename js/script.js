be76d2dc8bb97d7c0b20d506eefbbd73165c766d
Rasel Sharif <raselsharif9@gmail.com>
2023-07-11 15:42:19 +0600

    remove unnecessary code from js

--- js/script.js
+++ js/script.js
@@ -18,17 +18,17 @@ var mixer = mixitup(".mixitup", {
 // ========= section on top ======
 
 $(function () {
-  // ============ preloadder ============
+  // ============ preloader ============
   $(window).on("load", function () {
     $(".preloader__main").delay(700).fadeOut(700);
   })
-  // ========== nvbar icon change =========
+  // ========== navbar icon change =========
 
   $(".navbar-toggler-icon").click(function () {
     $(this).toggleClass("navbar_close");
   })
 
-  // ======= hide menubar click on body =========
+  // ======= hide menubar click on nav link =========
   $(".nav-link").click(function () {
     $("#navbarNav").removeClass("show")
     $(".navbar-toggler-icon").removeClass("navbar_close")
@@ -36,59 +36,48 @@ $(function () {
   // ========== section close =========
   $(".about__links").click(function () {
     $("#about").addClass("about__top")
-    // $("#about").fadeIn()    
   })
   $(".about__close").click(function () {
     $("#about").removeClass("about__top")
-    // $("#about").fadeOut()    
   })
   // ===================================
   $(".resume__links").click(function () {
     $("#resume").addClass("resume__top")
-    // $("#resume").fadeIn()
   })
   $(".resume__close").click(function () {
     $("#resume").removeClass("resume__top")
-    // $("#resume").fadeOut()
   })
   // ===================================
 
   $(".portfolio__links").click(function () {
     $("#portfolio").addClass("portfolio__top")
-    // $("#portfolio").fadeIn()
   })
   $(".portfolio__close").click(function () {
     $("#portfolio").removeClass("portfolio__top")
-    // $("#portfolio").fadeOut()
   })
   // ===================================
 
   $(".blog__links").click(function () {
 
     $("#blog").addClass("blog__top")
-    // $("#blog").fadeIn()
   })
   $(".blog__close").click(function () {
 
     $("#blog").removeClass("blog__top")
-    // $("#blog").fadeOut()
   })
   // ===================================
 
   $(".contact__links").click(function () {
 
     $("#contact").addClass("contact__top")
-    // $("#contact").fadeIn()
   })
   $(".contact__close").click(function () {
 
     $("#contact").removeClass("contact__top")
-    // $("#contact").fadeOut()
   })
 
   // ========== portfolio active menu ========
   $(".list__btn").click(function () {
-    console.log("clicked");
     $(this).addClass("btn__active").siblings().removeClass("btn__active");
   })
 
@@ -182,92 +171,6 @@ function SendEmail() {
     });
   }
 }
-
-
-// function SendEmail() {
-//   let username = document.querySelector(".name");
-//   let email = document.querySelector(".email");
-//   let subject = document.querySelector(".subject");
-//   let text = document.querySelector(".text");
-//   let nameerr = document.querySelector(".name__err");
-//   let mailerr = document.querySelector(".mail__err");
-//   let suberr = document.querySelector(".sub__err");
-//   let texterr = document.querySelector(".text__err");
-//   let mail_valid = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-//   if (username.value === "") {
-//     nameerr.innerHTML = "Type your full name";
-//     nameerr.style.color = "red";
-//     username.style.borderBottom = "2px solid red";
-//   } else {
-//     nameerr.innerHTML = "";
-//     username.style.borderBottom = "";
-//   }
-
-//   if (email.value === "") {
-//     mailerr.innerHTML = "Type your valid email";
-//     mailerr.style.color = "red";
-//   } else {
-//     if (email.value.match(mail_valid)) {
-//       mailerr.innerHTML = "";
-//     } else {
-//       mailerr.innerHTML = "Your email is not valid";
-//     }
-//   }
-
-//   if (subject.value === "") {
-//     suberr.innerHTML = "Type a subject";
-//     suberr.style.color = "red";
-//   } else {
-//     suberr.innerHTML = "";
-//   }
-
-//   if (text.value === "") {
-//     texterr.innerHTML = "Type your message";
-//     texterr.style.color = "red";
-//   } else {
-//     texterr.innerHTML = "";
-//   }
-
-//   // Check if all fields are valid before sending the email
-//   if (nameerr.innerHTML === "" && mailerr.innerHTML === "" && suberr.innerHTML === "" && texterr.innerHTML === "") {
-//     // Configure SMTPJS with your SMTP service provider credentials
-//     Email.send({
-//       SecureToken: "4e8111f5-eaf4-4d98-aeb7-392791102ad2", // Replace with your SMTPJS token
-//       To: "raselshrf2016@gmail.com",
-//       From: email.value,
-//       Subject: subject.value,
-//       Body: text.value,
-//     }).then(function (message) {
-//       if (message === "OK") {
-//         alert("Email sent successfully!");
-//         // Clear input fields
-//         username.value = "";
-//         email.value = "";
-//         subject.value = "";
-//         text.value = "";
-//       } else {
-//         alert("Error sending email. Please try again later.");
-//       }
-//     });
-//   }
-// }
-// function sendEmail(){
-//   Email.send({
-//     SecureToken : "4e8111f5-eaf4-4d98-aeb7-392791102ad2",
-//     To : email.value,
-//     From : 'raselshrf2016@gmail.com',
-//     Subject : subject.value,
-//     Body : text.value
-// }).then(
-//   message => alert(message)
-// );
-// }
-
-
-
-
-
 
 // SecureToken: "4e8111f5-eaf4-4d98-aeb7-392791102ad2"
 
