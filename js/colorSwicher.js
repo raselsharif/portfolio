9d590141fd17c08e280788abe6bdd2bd43de81be
Rasel Sharif <raselsharif9@gmail.com>
2023-08-07 13:19:41 +0600

    added white mode

--- js/colorSwicher.js
+++ js/colorSwicher.js
@@ -2,11 +2,7 @@
 $(function () {
     $("#white").click(white);
 
-    // function black() {
-    //     $("#banner, .banner__main__heading,p,a,h1,h2,h3,h4,h5,h6,#about,#resume,#portfolio,#blog,#contact").addClass("black");
-    // }
-
     function white() {
-        $("#banner, .banner__main__heading,p,a,h1,h2,h3,h4,h5,h6,#about,#resume,#portfolio,#blog,#contact").toggleClass("white");
+        $("#banner, .banner__main__heading,p,a,h1,h2,h3,h4,h5,h6,#about,#resume,#portfolio,#blog,#contact,.about__card,.testi__card,.plan__card, .form input, textarea, .slick-dots button,.resume__card,.close__btn,.list__main, .resume__items").toggleClass("white");
     }
 })
