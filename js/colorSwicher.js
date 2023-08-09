affa07140079351b79e23a7b571b5b8fa4805a67
Rasel Sharif <raselsharif9@gmail.com>
2023-08-09 13:06:20 +0600

    fixed toogle menu color for mobile device

--- js/colorSwicher.js
+++ js/colorSwicher.js
@@ -3,6 +3,6 @@ $(function () {
     $("#white").click(white);
 
     function white() {
-        $("#banner, .banner__main__heading,p,a,h1,h2,h3,h4,h5,h6,#about,#resume,#portfolio,#blog,#contact,.about__card,.testi__card,.plan__card, .form input, textarea, .slick-dots button,.resume__card,.close__btn,.list__main, .resume__items, .color__switch, .show").toggleClass("white");
+        $("#banner, .banner__main__heading,p,a,h1,h2,h3,h4,h5,h6,#about,#resume,#portfolio,#blog,#contact,.about__card,.testi__card,.plan__card, .form input, textarea, .slick-dots button,.resume__card,.close__btn,.list__main, .resume__items, .color__switch,.navbar").toggleClass("white");
     }
 })
